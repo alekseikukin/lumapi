5dcb141b20dc9d182719fde257d22995bcc888cd
alekseikukin <bnax.gm@gmail.com>
2022-02-03 10:10:59 +0200

    extra 'SendComand' was delited from getTransmitance

--- getTransmitance.m
+++ getTransmitance.m
@@ -1,20 +1,16 @@
 function Transmittance = getTransmitance(session,monitor)
-
+comand = {};
 mainVarT = ['Transmittance_' monitor];
 varT = ['Transmittance_' monitor '_T'];
 varLambda = ['Transmittance_' monitor '_lambda'];
 
-comand = [mainVarT '= getresult("' monitor '", "T");' ];
-session.SendComand(comand);
-
-comand = [varT '=' mainVarT '.T;' ];
-session.SendComand(comand);
-
-comand = [varLambda '=' mainVarT '.lambda;' ];
+comand(end+1) = [mainVarT '= getresult("' monitor '", "T");' ];
+comand(end+1) = [varT '=' mainVarT '.T;' ];
+comand(end+1) = [varLambda '=' mainVarT '.lambda;' ];
 session.SendComand(comand);
 
 Transmittance = session.GetVar(varT);
-Transmittance(:, end+1) = (session.GetVar(varLambda))*1e6;
+Transmittance(:, end+1) = (session.GetVar(varLambda)) * 1e6;
 
 end
 
